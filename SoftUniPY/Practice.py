6d4836e1da46e78be62eaa1579198d192106af7e
Blagovest Borukov <blagovest@gridbit.tech>
2025-02-26 12:11:46 +0200

    Add changes

--- SoftUniPY/Practice.py
+++ SoftUniPY/Practice.py
@@ -1,13 +1,52 @@
-age = int(input())
-gender = input()
+product = input()
+city = input()
+amount = float(input())
 
-if gender == 'f':
-    if age < 16:
-        print('Miss')
-    else:
-        print('Ms.')
-else:
-    if age < 16:
-        print('Master')
-    else:
-        print('Mr.')
+if city == 'Sofia':
+    if product == 'coffee':
+        price = amount * 0.5
+        print(f'{price:.1f}')
+    elif product == 'water':
+        price = amount * 0.8
+        print(f'{price:.1f}')
+    elif product == 'beer':
+        price = amount * 1.2
+        print(f'{price:.1f}')
+    elif product == 'sweets':
+        price = amount * 1.45
+        print(f'{price:.1f}')
+    elif product == 'peanuts':
+        price = amount * 1.6
+        print(f'{price:.1f}')
+elif city == 'Plovdiv':
+    if product == 'coffee':
+        price = amount * 0.4
+        print(f'{price:.1f}')
+    elif product == 'water':
+        price = amount * 0.7
+        print(f'{price:.1f}')
+    elif product == 'beer':
+        price = amount * 1.15
+        print(f'{price:.1f}')
+    elif product == 'sweets':
+        price = amount * 1.30
+        print(f'{price:.1f}')
+    elif product == 'peanuts':
+        price = amount * 1.5
+        print(f'{price:.1f}')
+elif city == 'Varna':
+    if product == 'coffee':
+        price = amount * 0.45
+        print(f'{price:.1f}')
+    elif product == 'water':
+        price = amount * 0.7
+        print(f'{price:.1f}')
+    elif product == 'beer':
+        price = amount * 1.1
+        print(f'{price:.1f}')
+    elif product == 'sweets':
+        price = amount * 1.35
+        print(f'{price:.1f}')
+    elif product == 'peanuts':
+        price = amount * 1.55
+        print(f'{price:.1f}')
