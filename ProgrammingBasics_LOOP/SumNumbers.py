0f130ce15a19bf142d23052fb641bbeea096fe50
Blagovest Borukov <blagovest@gridbit.tech>
2025-02-27 16:42:11 +0200

    Add

---
+++ ProgrammingBasics_LOOP/SumNumbers.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(input())
+max_num = -sys.maxsize
+sum = 0
+
+for i in range(0, n):
+    num = int(input())
+
+    if num > max_num:
+        max_num = num
+
+    sum += num
+
+if max_num == sum - max_num:
+    print('Yes')
+    print(f'Sum = {max_num}')
+else:
+    sum = sum - max_num
+    print('No')
+    print(f'Diff = {abs(max_num - sum)}')
