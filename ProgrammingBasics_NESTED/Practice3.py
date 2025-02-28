5aa814192640e60edebbb71bd5f033c949c32acd
Blagovest Borukov <blagovest@gridbit.tech>
2025-02-28 16:40:00 +0200

    Add

---
+++ ProgrammingBasics_NESTED/Practice3.py
@@ -0,0 +1,12 @@
+floors = int(input())
+rooms = int(input())
+
+for i in range(floors, 0, -1):
+    for j in range(0, rooms):
+        if i == floors:
+            print("L{0}{1}".format(i, j), end= " ")
+        elif i % 2 == 0:
+            print("O{0}{1}".format(i, j), end= " ")
+        elif i % 2 != 0:
+            print("A{0}{1}".format(i, j), end= " ")
+    print('')
